ea11fc562a6c299f665a44d0e16a9098dd7058ee
Ambika Sahu <90752801+AmbikaSahu2577@users.noreply.github.com>
2023-01-03 11:24:49 +0530

    Add files via upload

---
+++ AlfhabatePattern_2.java
@@ -0,0 +1,14 @@
+public class AlfhabatePattern_2
+{
+	public static void main(String[] args) 
+	{
+		for (char i = 'A';i<= 'D'; i++) 
+		{
+			for (char j=i;j>='A' ;j--) 
+			{
+			   System.out.print(j);	
+			}
+			System.out.println();
+		}
+	}
+}
